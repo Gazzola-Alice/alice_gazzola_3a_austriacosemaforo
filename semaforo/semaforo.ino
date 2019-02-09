98d17f51b49151d851ccd9949bade2d4649324f9
Alice <alice.gazzola@barsanti.edu.it>
2019-02-09 10:54:28 +0100

    9/02/2019 apportate le ultime modifiche

--- semaforo/semaforo.ino
+++ semaforo/semaforo.ino
@@ -4,72 +4,206 @@ int ledgiallo=7;
 int ledverdedue= 8;
 int ledgiallodue= 10;
 int ledrossodue=4;
-int cento=100;
-int mille=1000;
-int diecimila=10000;
+int i;
+int lampeggio;
+int numero;
+int tempo;
+int t;
+int nl;
+int pausa=100;
+
 void setup() {
-  // put your setup code here, to run once:
 pinMode(ledverde,OUTPUT);
 pinMode(ledrosso,OUTPUT);
 pinMode(ledgiallo,OUTPUT);
 pinMode(ledverdedue,OUTPUT);
 pinMode(ledrossodue,OUTPUT);
 pinMode(ledgiallodue,OUTPUT);
-
+Serial.begin(9600);
+Serial.println("seriale attiva");
+chiedogiallo();
+chiedolamp();
+chiedonlampeggi();
+chiedoloop();
 }
-void semaforo (){ digitalWrite(ledrosso,HIGH);
-   digitalWrite(ledrossodue,HIGH);
-  delay (diecimila);
-    digitalWrite(ledrosso,HIGH); //turn the led on (high is the voltage level)
-     digitalWrite(ledgiallo,HIGH);
-     digitalWrite(ledrossodue,HIGH); //turn the led on (high is the voltage level)
-     digitalWrite(ledgiallodue,HIGH); 
-     delay (diecimila);
-     digitalWrite(ledrosso,LOW); //turn the led on (high is the voltage level)
-     digitalWrite(ledgiallo,LOW);
-      digitalWrite(ledrossodue,LOW); //turn the led on (high is the voltage level)
-     digitalWrite(ledgiallodue,LOW);
-        delay(cento);
-        digitalWrite(ledverde,HIGH);
-         digitalWrite(ledverdedue,HIGH);//turn the led on (high is the voltage level)
- //aspetta per un secondo
- delay(diecimila);
-  digitalWrite(ledverde,LOW);//turn the led off
-   digitalWrite(ledverdedue,LOW);
-   delay(1000);
-digitalWrite(ledverde,HIGH); //turn the led on (high is the voltage level)
- digitalWrite(ledverdedue,HIGH);
-  delay(mille);//aspetta per un secondo
-  digitalWrite(ledverde,LOW);//turn the led off
-   digitalWrite(ledverdedue,LOW);
-   delay(cento);
-   digitalWrite(ledverde,HIGH); //turn the led on (high is the voltage level)
-    digitalWrite(ledverdedue,HIGH);
-  delay(cento);//aspetta per un secondo
-  digitalWrite(ledverde,LOW);//turn the led off
-   digitalWrite(ledverdedue,LOW);
-   delay(cento);
-   digitalWrite(ledverde,HIGH); //turn the led on (high is the voltage level)
-    digitalWrite(ledverdedue,HIGH);
-  delay(mille);//aspetta per un secondo
-  digitalWrite(ledverde,LOW);//turn the led off
-   digitalWrite(ledverdedue,LOW);
-   delay(cento);
-   digitalWrite(ledverde,HIGH); //turn the led on (high is the voltage level)
-    digitalWrite(ledverdedue,HIGH);
-  delay(mille);//aspetta per un secondo
-  digitalWrite(ledverde,LOW);//turn the led off
-   digitalWrite(ledverdedue,LOW);
-   delay(cento);
+void chiedogiallo() {
+  Serial.println("Scrivi quanto deve valere l'intervallo di tempo del giallo");
+  while(Serial.available()==0);
+    if ( Serial.available ()> 0){
+  numero= Serial.readString().toInt();
+  Serial.println(numero);
+  }
+}
+void chiedolamp() {
+  Serial.println("Scrivi quanto deve valere il tempo di lampeggio del verde ");
+  while(Serial.available()==0);
+    if ( Serial.available ()> 0){
+  lampeggio= Serial.readString().toInt();
+  Serial.println(lampeggio);
+  }
+}
+void chiedonlampeggi(){
+  //devo chiedere quanti lampeggi voglio
+   Serial.println("Scrivi quante volte il led verde deve lampeggiare ");
+  while(Serial.available()==0);
+    if ( Serial.available ()> 0){
+ nl= Serial.readString().toInt();
+  Serial.println(nl);
+  }
+  
+  }
+  void chiedoloop (){
+    //devo chiedere quanto dura una fase o tutto il ciclo e poi divido il valore per le fasi tranne quella del giallo
+     Serial.println("Scrivi quanto deve durare il semaforo( quanto deve durare il rosso,il verde e il rosso e giallo) ");
+  while(Serial.available()==0);
+    if ( Serial.available ()> 0){
+tempo= Serial.readString().toInt();
+  Serial.println(tempo);
+  t= ((tempo-numero)/4);
+  errore();
+    }}
 
- digitalWrite(ledgiallo,HIGH); 
+void errore(){
+  if (tempo<numero+(lampeggio*nl))
+  {
+ Serial.println ("IL TEMPO DI DURATA DEL SEMAFORO DEVE ESSERE MAGGIORE DELLA SOMMA DEI VALORI INSERITI PRECEDENTEMENTE!!");
+ Serial.println("Ricominciamo");
+ delay(pausa);  
+   setup();
+ 
+   }
+   else{ Serial.println("Il semaforo parte!");
+    }
+}
+void psRosso() {
+  digitalWrite(ledgiallo,LOW);
+  digitalWrite(ledverde,LOW);
+  digitalWrite(ledrosso,HIGH);
+}
+
+void psGialloRosso() {
+  digitalWrite(ledverde,LOW);
+  
+  digitalWrite(ledrosso,HIGH);
+  digitalWrite(ledgiallo,HIGH);
+}
+
+void psVerde() {
+  digitalWrite(ledrosso,LOW);
+  digitalWrite(ledgiallo,LOW);
+  
+  digitalWrite(ledverde,HIGH);
+}
+
+void psLampeggiaVerde() {digitalWrite(ledverde,LOW);
+    digitalWrite(ledrosso,LOW);
+    digitalWrite(ledgiallo,LOW);
+    i=0;
+      while (i<nl){
+    digitalWrite(ledverdedue,HIGH);
+    delay(lampeggio);
+
+   digitalWrite(ledverdedue,LOW);
+    delay(lampeggio);
+    i++;}
+  
+   
+}
+
+void psGiallo() {
+  digitalWrite(ledverde,LOW);
+  digitalWrite(ledrosso,LOW);
+  
+  digitalWrite(ledgiallo,HIGH);
+}
+  
+
+void ssRosso() {
+  digitalWrite(ledgiallodue,LOW);
+  digitalWrite(ledverdedue,LOW);
+  
+  digitalWrite(ledrossodue,HIGH);
+}
+
+void ssGialloRosso() {
+  digitalWrite(ledverdedue,LOW);
+  
+  digitalWrite(ledrossodue,HIGH);
   digitalWrite(ledgiallodue,HIGH);
-     delay (diecimila);
-      digitalWrite(ledgiallo,LOW);
-       digitalWrite(ledgiallodue,LOW);
-      delay (cento);
+}
+
+void ssVerde() {
+  digitalWrite(ledrossodue,LOW);
+  digitalWrite(ledgiallodue,LOW);
+  
+  digitalWrite(ledverdedue,HIGH);
+}
+
+void ssLampeggiaVerde() {
+    digitalWrite(ledrossodue,LOW);
+    digitalWrite(ledgiallodue,LOW);
+    digitalWrite(ledverdedue,LOW);
+   // delay(lampeggio);
+   i=0;
+   while (i<nl){
+    digitalWrite(ledverdedue,HIGH);
+    delay(lampeggio);
+
+   digitalWrite(ledverdedue,LOW);
+    delay(lampeggio);
+    i++;
+  
+}}
+
+void ssGiallo() {
+  digitalWrite(ledverdedue,LOW);
+  digitalWrite(ledrossodue,LOW);
+  
+  digitalWrite(ledgiallodue,HIGH);
+}
+
+
+void faseuno(){
+   ssRosso();
+  psVerde();
+  delay(t);
+  
+  }
+  void fasedue(){
+     ssGialloRosso();
+  psLampeggiaVerde();
+  delay(t);
+    }
+    void fasetre()
+    { ssVerde();
+  psRosso();
+  delay(t);
+      }
+      void fasequattro(){
+         ssLampeggiaVerde();
+  psGialloRosso();
+  delay(t);
+        
+        }
+        void fasecinque()
+        {
+           ssGiallo();
+  delay(numero);
+          }
+void mysemaforo() { 
+  
+  faseuno();
+  
+ fasedue();
+  
+ fasetre();
+  
+fasequattro();
+ fasecinque(); 
+ 
 }
 void loop() {
- semaforo();
-
+ 
+ mysemaforo();
+ 
 }
